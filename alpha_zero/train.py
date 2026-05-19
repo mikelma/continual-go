1d04ebdb19e7aa1d393bb5a08e3f7e0ccfca564d
Mikel <mikelma@tutanota.com>
2026-05-19 14:49:59 -0600

    Partial implementation of test.py

--- alpha_zero/train.py
+++ alpha_zero/train.py
@@ -17,40 +17,11 @@ from pydantic import BaseModel
 import tyro
 
 from network import AZNet
+from config import Config
 from continual_go import ContinualGo, State
 
 devices = jax.local_devices()
 num_devices = len(devices)
-
-
-class Config(BaseModel):
-    board_size: int = 9
-    max_stones: int = 32
-
-    wandb: bool = True
-
-    seed: int = 42
-    max_num_iters: int = 400
-
-    # network params
-    num_channels: int = 128
-    num_layers: int = 6
-    resnet_v2: bool = True
-
-    # selfplay params
-    selfplay_batch_size: int = 1024
-    num_simulations: int = 32
-    max_num_steps: int = 256
-
-    # training params
-    training_batch_size: int = 4096
-    learning_rate: float = 0.001
-    gamma: float = 0.99  # discount factor for the continuing/infinite-horizon return
-
-    # eval params
-    eval_interval: int = 5
-
-    save_interval: int = 5
 
 config = tyro.cli(Config)
 env = ContinualGo(size=config.board_size, k=config.max_stones)
